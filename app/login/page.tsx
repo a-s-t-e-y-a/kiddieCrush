05172595e4ea295812c6ea286ea823123ab202b2
shivamsinghcse <shivamsinghcse21@gmail.com>
2024-02-09 10:20:59 +0530

    Login

---
+++ app/login/page.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import sideImage from "../../public/images/sticker_13.png";
+import { ArrowRight } from "lucide-react";
+
+function Login() {
+  return (
+    <div className="bg-amber-100 w-[100vw] h-[100vh] absolute">
+      <div className="md:w-[70%] h-[350px] bg-white absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] grid md:grid-cols-2 rounded-md">
+        <div className="">
+          <Image
+            src={sideImage}
+            alt="cute relations"
+            className="w-[70%] mx-auto"
+          ></Image>
+        </div>
+        <div className="">
+          <h1 className="text-2xl my-5 ">Signin</h1>
+          <form>
+            <div className="w-72">
+              <div className="relative w-full min-w-[200px] h-10">
+                <input
+                  type="text"
+                  className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
+                  placeholder=" "
+                />
+                <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">
+                  Username
+                </label>
+              </div>
+            </div>
+            <div className="w-72 my-5">
+              <div className="relative w-full min-w-[200px] h-10">
+                <input
+                  type="password"
+                  className="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-2.5 rounded-[7px] border-blue-gray-200 focus:border-gray-900"
+                  placeholder=" "
+                />
+                <label className="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal !overflow-visible truncate peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-sm text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[3.75] text-gray-500 peer-focus:text-gray-900 before:border-blue-gray-200 peer-focus:before:!border-gray-900 after:border-blue-gray-200 peer-focus:after:!border-gray-900">
+                  Password
+                </label>
+              </div>
+            </div>
+            <button className="w-72 flex border-solid border-gray-500 rounded-md border-2 p-2 hover:gap-2 transition transform duration-300 hover:scale-105 group">
+              Signin <ArrowRight className="group-hover:hidden"></ArrowRight>
+              <span className="hidden group-hover:inline">❤️</span>
+            </button>
+          </form>
+          <button className="w-72 my-5 flex border-solid border-white rounded-md border-2 p-2 hover:gap-2 transition transform duration-300 hover:scale-105 group">
+              Signup <ArrowRight className="group-hover:hidden"></ArrowRight>
+              <span className="hidden group-hover:inline">😍</span>
+            </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Login;
